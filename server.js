f2e53399e8817b37dca4a0afd3b9d63d1da74919
Steve Burton <steveburtonwebdev@gmail.com>
2019-08-19 11:27:41 +0100

    inital code commit

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : 'localhost',
+      user : '',
+      password : '',
+      database : 'facebrain'
+    }
+  });
+
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password:  'cookies',
+            entries: 0,
+            joined: new Date(),
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password:  'bananas',
+            entries: 0,
+            joined: new Date(),
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+          res.json(database.users[0]);  
+        } else {
+            res.status(400).json('error logging in');
+        }
+});
+
+app.post('/register', (req, res) => {
+    const { email, name, password} = req.body;
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+    }).then(user => {
+        res.json(user[0])
+    })
+    .catch(err => res.status(400).json('Unable to Register'))
+});
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('user not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('user not found');
+    }
+
+})
+
+app.listen(3001, () => {
+    console.log('app is running on port 3001');
+});
